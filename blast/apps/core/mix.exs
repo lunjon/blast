425c55718e08946e26bb331b74640e6aab942c94
Jonathan Lundholm <jon.lundholm@gmail.com>
2022-09-22 21:48:50 +0200

    fix: umbrella apps

--- blast/apps/core/mix.exs
+++ blast/apps/core/mix.exs
@@ -5,22 +5,21 @@ defmodule Core.MixProject do
     [
       app: :core,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      aliases: aliases()
+      deps: deps()
     ]
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
-    ]
-  end
-
-  defp aliases do
-    [
-      all: ["compile", "format", "escript.build"]
+      extra_applications: [:logger],
+      mod: {Core.Application, []}
     ]
   end
 
